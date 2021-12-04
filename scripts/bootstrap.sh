f8eac32768c99463db4ebe40016d3ea44eb325a2
sandeep-gh <gupta.sandeep@gmail.com>
2021-12-04 17:04:44 +0530

    updated

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,10 +1,8 @@
 pass=$1
 #syspatch
 adpass=`encrypt $pass`
-useradd -m -L staff -p $adpass adming
+useradd -m -L staff -p \"$adpass\" -G wheel adming
 usermod -G wheel adming
 echo "permit nopass :wheel" >>/etc/doas.conf
 pkg_add -uvi
-su adming
-doas pkg_add python3
-groupadd -g 1002 shared
+su adming -c "doas pkg_add python3; groupadd -g 1002 shared"
